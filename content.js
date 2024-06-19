3f11e3fb7d16ff294b4bf29d626e3184adb71c48
anhhuy294 <anhhuy2099@gmail.com>
2024-06-19 11:28:12 +0700

    mofify style

--- content.js
+++ content.js
@@ -1,44 +1,49 @@
 let on = true;
+
+const handleAddLayer = () => {
+  const secondaryTab = document.querySelector("#fixed-columns-secondary #secondary");
+  const primaryBelow = document.querySelector("#primary #below");
+  const oldLayer = document.getElementById("primary-below-layer");
+
+  let mode = "dark";
+  const bg = getComputedStyle(document.documentElement).getPropertyValue("--yt-spec-base-background").trim();
+  if (bg.includes("#fff")) mode = "light";
+
+  if (window.scrollY === 0 && on && secondaryTab && primaryBelow) {
+    if (mode === "light") primaryBelow.style.opacity = 0.3;
+    if (oldLayer) return oldLayer;
+    const layer = document.createElement("div");
+    layer.id = "primary-below-layer";
+
+    const bgColor = mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(0,0,0,0.2)";
+    layer.style.cssText = `position: absolute; top: 12px; left: -2px; width: calc(100% + 4px); height: 300px; background-color: ${bgColor}; filter: blur(3px); border-radius: 12px; z-index: 1000`;
+    primaryBelow.appendChild(layer);
+    return layer;
+  }
+
+  if (mode === "light") primaryBelow.style.opacity = 1;
+  if (oldLayer) primaryBelow.removeChild(oldLayer);
+};
+
+//catching on
 chrome.runtime.onMessage.addListener(function (message) {
   if (message.action === "toggleSwitch") {
     on = message.on;
+    handleAddLayer();
   }
 });
 
+//handle observe youtube elements loading when primary element loaded
+let added = false;
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.type === "childList") {
       const targetElement = document.getElementById("primary");
-      if (targetElement) {
-        console.log("mounted", { on });
-
-        const handleAddHider = () => {
-          const secondaryTab = document.querySelector("#fixed-columns-secondary #secondary");
-          const primaryBelow = document.querySelector("#primary #below");
-          const oldHider = document.getElementById("primary-below-hider");
-
-          let mode = "dark";
-          const bg = getComputedStyle(document.documentElement).getPropertyValue("--yt-spec-base-background").trim();
-          if (bg.includes("#fff")) mode = "light";
-
-          if (window.scrollY === 0 && on && secondaryTab && primaryBelow) {
-            if (oldHider) return;
-            const hider = document.createElement("div");
-            hider.id = "primary-below-hider";
-
-            const bgColor = mode === "dark" ? "#000" : "rgba(0,0,0,0.6)";
-            hider.style.cssText = `position: absolute; top: 10px; left: -5px; width: calc(100% + 10px); height: 250px; background-color: ${bgColor}; opacity: 0.8; filter: blur(5px); border-radius: 12px; z-index: 1000`;
-            primaryBelow.appendChild(hider);
-            return;
-          }
-
-          if (oldHider) primaryBelow.removeChild(oldHider);
-        };
-
-        handleAddHider();
-        window.addEventListener("scroll", handleAddHider);
-
+      if (targetElement && !added) {
+        handleAddLayer();
+        window.addEventListener("scroll", handleAddLayer);
         observer.disconnect();
+        added = true;
       }
     }
   });
